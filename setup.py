0a7d783f728cc61e1f45fa75c9269b2da9f2570d
mikjf <michael.jean.flury@gmail.com>
2022-11-22 16:20:53 +0100

    first commit - standard data science project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Our goal is to analyse the business context behind the provided dataset, propose a methodology for forecasting future transaction levels and develop a solution that can run and update automatically.',
+    author='Alžbeta Bohiniková, Luis Miguel Rodríguez Sedano, Mukund Pondkule, Michael Flury',
+    license='',
+)
